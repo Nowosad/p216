30e44425d3784d2c26b6762e346ba54432b77c4a
j_n <tupiszakaczki@gmail.com>
2015-11-28 12:46:18 +0100

    new functions

---
+++ R/mt_changer.R
@@ -0,0 +1,24 @@
+#' Funkcja mt_changer
+#'
+#' Ta funkcja zamienia wartości parametrów w wierszach 16, 18, 22, 23
+#' w folderze zawierającym pliki o rozszerzeniu RDC.
+#'
+#' @param folder_path ścieżka do folderu zawierającego pliki RDC
+#' @param t1 tekst do zastąpienia wartości w wierszach 16 i 18
+#' @param t2 tekst do zastąpienia wartości w wierszu 22
+#' @param t3 tekst do zastąpienia wartości w wierszu 22
+#'
+#' @return nowe pliki ze zmienionymi wartościami.
+#' Nowy plik będzie miał taką samą nazwę z dopiskiem _new
+#'
+#' @export
+#'
+#' \donttest{
+#'    folder <- "C:/Users/jakub/pliki_RDC"
+#'    mt_changer(folder, "7500.0000000", "0.0000000", "Background")
+#' }
+
+mt_changer <- function(folder_path, ...){
+    files <- normalizePath(list.files(folder_path, pattern="*.RDC" , full.names = TRUE))
+    lapply(files, text_changer, t1, t2, t3)
+}
--- R/text_changer.R
+++ R/text_changer.R
@@ -1,26 +1,21 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
-}
-
-file <- "raw_data/przyklady_plikow_dla_temperatury_powierzchni_(LST)/p_mod11a2_a2000065_h18v01_005_2007176171013_lst_day_1km.RDC"
-t1 <- "7500.0000000"
-t2 <- "0.0000000"
-t3 <- "Background"
+#' Funkcja text_changer
+#'
+#' Ta funkcja zamienia wartości parametrów w wierszach 16,
+#' 18, 22, 23 w plikach o rozszerzeniu RDC.
+#'
+#' @param file ścieżka do pliku RDC
+#' @param t1 tekst do zastąpienia wartości w wierszach 16 i 18
+#' @param t2 tekst do zastąpienia wartości w wierszu 22
+#' @param t3 tekst do zastąpienia wartości w wierszu 22
+#'
+#' @return nowy plik ze zmienionymi wartościami.
+#' Nowy plik będzie miał taką samą nazwę z dopiskiem _new
+#' @export
+#'
+#' \donttest{
+#'    file <- "raw_data/przyklady_plikow_dla_temperatury_powierzchni_(LST)/p_mod11a2_a2000065_h18v01_005_2007176171013_lst_day_1km.RDC"
+#'    text_changer(file, "7500.0000000", "0.0000000", "Background")
+#' }
 
 text_changer <- function(file, t1, t2, t3){
     full_path <- normalizePath(file)
@@ -34,13 +29,3 @@ text_changer <- function(file, t1, t2, t3){
     name <- gsub("^[.]*|[.][^.]*$", "", basename(full_path), perl = TRUE)
     writeLines(text1, con = paste0(dirname(full_path), "/", name, "_new", ".RDC"), sep = "\n", useBytes = FALSE)
 }
-
-text_changer(file, t1, t2, t3)
-
-
-mt_changer <- function(folder_path, ...){
-    files <- normalizePath(list.files(folder_path, full.names = TRUE))
-    lapply(files, text_changer, t1, t2, t3)
-}
-
-mt_changer("/home/jakub/Documents/change_doc/raw_data/przyklady_plikow_dla_temperatury_powierzchni_(LST)", t1, t2, t3)
